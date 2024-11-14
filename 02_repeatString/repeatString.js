5c2de8f808ee6e5ff224fc3e011bc44b24a7e60e
ev-moon <evmoon77@gmail.com>
2024-11-13 20:30:39 -0800

    exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, numRepeat) {
+  if (numRepeat < 0) {
+    return "ERROR";
+  }
+  result = "";
+  for (let i = 0; i < numRepeat; i++) {
+    result += word;
+  }
+  return result;
 };
 
 // Do not edit below this line
