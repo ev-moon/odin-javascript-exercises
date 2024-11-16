c71cea8940909e5fc8376050025ef870e28a462f
ev-moon <evmoon77@gmail.com>
2024-11-15 20:45:33 -0800

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, ...valuesToRemove) {
+  return inputArray.filter(
+    (arrayElement) => !valuesToRemove.includes(arrayElement)
+  );
 };
 
 // Do not edit below this line
