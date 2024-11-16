464a98c6dad9c592271c1934eaa9453cd74a6a9b
ev-moon <evmoon77@gmail.com>
2024-11-15 20:52:58 -0800

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (int1, int2) {
+  if (!Number.isInteger(int1) || !Number.isInteger(int2)) {
+    return "ERROR";
+  }
+  let n1 = Math.min(int1, int2);
+  let n2 = Math.max(int1, int2);
+  if (n1 < 0 || n2 < 0) {
+    return "ERROR";
+  }
+  let sum = 0;
+  for (let i = n1; i <= n2; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
